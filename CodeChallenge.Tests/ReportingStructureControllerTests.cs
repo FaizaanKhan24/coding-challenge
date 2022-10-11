c68cd435cdb6c6a7d31ea03bb7a80363413d1c98
Faizaan Khan <faizaankhan24@gmail.com>
2022-10-11 14:30:27 -0400

    Create ReportingStructureControllerTests.cs

---
+++ CodeChallenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using CodeChallenge.Models;
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetReportingStructureById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedFirstName = "John";
+            var expectedLastName = "Lennon";
+            var expectedReportingPerson = 4;
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reporting/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+            Assert.IsNotNull(reportingStructure.ReportingStructureId);
+            Assert.AreEqual(expectedFirstName, reportingStructure.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, reportingStructure.Employee.LastName);
+            Assert.AreEqual(employeeId, reportingStructure.Employee.EmployeeId);
+            Assert.AreEqual(expectedReportingPerson, reportingStructure.NumberOfReport);
+        }
+    }
+}
+
